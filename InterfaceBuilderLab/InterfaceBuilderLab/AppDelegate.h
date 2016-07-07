fe30605fa06bdf231ecfd661b7d846badef33f5c
ugowe <joe@spheares.com>
2016-07-07 10:04:14 -0400

    add code to Interface Builder Lab

---
+++ InterfaceBuilderLab/InterfaceBuilderLab/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InterfaceBuilderLab
+//
+//  Created by Ugowe on 7/7/16.
+//  Copyright © 2016 Ugowe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InterfaceBuilderLab/InterfaceBuilderLab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InterfaceBuilderLab
+//
+//  Created by Ugowe on 7/7/16.
+//  Copyright © 2016 Ugowe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
